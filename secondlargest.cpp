a65de94da3547c38c5e610d706b480bc3b721eea
harsh-choudhary-nature <108018451+harsh-choudhary-nature@users.noreply.github.com>
2022-08-22 22:40:27 +0530

    Update secondlargest.cpp

--- secondlargest.cpp
+++ secondlargest.cpp
@@ -1,21 +1,20 @@
 #include<stdio.h>
 int secondlargest(int arr[],int arr_size){
-    int maxi=arr[0];
+    int maxi=arr[0],secondMax=arr[0];
     for(int i=0;i<arr_size;i++){
-        if(arr[i]>maxi){
+        if((arr[i]>maxi)||(arr[i]>secondMax)){
+            if((arr[i]<maxi)&&(arr[i]>secondMax)){
+                secondMax=arr[i];
+                continue;
+            }
+            secondMax=maxi;
             maxi=arr[i];
-        }
-    }
-    int secondMax=arr[0];
-    for(int i=0;i<arr_size;i++){
-        if((arr[i]>secondMax)&&(arr[i]<maxi)){
-            secondMax=arr[i];
         }
     }
     return secondMax;
 }
 int main(){
-    int arr[]={1,2,3,4,5,2,1,6,8,4,7};
+    int arr[]={1,2,3,4,5,2,1,6,8,4,7,9,5};
     printf("%d is the second maximum",secondlargest(arr,sizeof(arr)/sizeof(arr[0])));
     return 0;
 }
