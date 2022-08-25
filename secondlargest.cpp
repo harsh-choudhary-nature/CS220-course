c81bcaa2066cafd7ca5b4de7cba69f01c7d5a7cc
harsh-choudhary-nature <108018451+harsh-choudhary-nature@users.noreply.github.com>
2022-08-25 08:42:49 +0530

    Update secondlargest.cpp

--- secondlargest.cpp
+++ secondlargest.cpp
@@ -1,20 +1,32 @@
 #include<stdio.h>
 int secondlargest(int arr[],int arr_size){
-    int maxi=arr[0],secondMax=arr[0];
+    int maxi=arr[0];
+    int sec=arr[0];
+    int i=1;
+    while((sec==maxi)&&(i<arr_size)){
+        printf("%d\n",i);
+        sec=arr[i];
+        i++;
+    }
+    if(sec==maxi){
+        printf("Second amx not exists");
+        return -101010101010101;    //any random number
+    }
+
     for(int i=0;i<arr_size;i++){
-        if((arr[i]>maxi)||(arr[i]>secondMax)){
-            if((arr[i]<maxi)&&(arr[i]>secondMax)){
-                secondMax=arr[i];
+        if(((arr[i]>maxi)||(arr[i]>sec))&&(arr[i]!=maxi)){
+            if(arr[i]<maxi){
+                sec=arr[i];
                 continue;
             }
-            secondMax=maxi;
+            sec=maxi;
             maxi=arr[i];
         }
     }
-    return secondMax;
+    return sec;
 }
 int main(){
-    int arr[]={1,2,3,4,5,2,1,6,8,4,7,9,5};
+    int arr[]={15,15,15,2,3,4,5,2,1,6,8,4,7,9,15,15,15,14};
     printf("%d is the second maximum",secondlargest(arr,sizeof(arr)/sizeof(arr[0])));
     return 0;
 }
